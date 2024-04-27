fea5f7ba05841befe3d4b146e943faf966aa2009
2814109 <2814109k@gmail.com>
2024-04-27 14:30:57 +0900

    use useHooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,59 @@
 // @ts-ignore
-import { useState, useActionState, useOptimistic } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useActionState, useOptimistic, use, cache } from "react";
+import { createContext, Suspense } from "react";
 
-function App() {
-  const [name, setName] = useState(0);
-  const [optimisticName, setOptimisticName] = useOptimistic(name);
-  const [error, submitAction, isPending] = useActionState(async () => {
-    const error = false;
-    if (error) {
-      // You can return any result of the action.
-      // Here, we return only the error.
-      return error;
-    }
+const SiteContext = createContext({ name: "" });
 
-    // handle success
-  }, 0);
+type PodcastShow = {
+  title: string;
+};
+function Footer() {
+  const { name } = use(SiteContext);
+  return (
+    <footer>
+      <p>©{name}</p>
+    </footer>
+  );
+}
+
+const Podcast = cache(() => {
+  const podcast = use(
+    fetch("https://syntax.fm/api/shows/latest").then(
+      (x) => x.json() as Promise<PodcastShow>
+    )
+  );
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        {/* <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button> */}
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>{podcast.title}</h2>
+      <meta property="og:title" content={podcast.title} />
     </>
+  );
+});
+function App() {
+  // const [name, setName] = useState(0);
+  // const [optimisticName, setOptimisticName] = useOptimistic(name);
+  // const [error, submitAction, isPending] = useActionState(async () => {
+  //   const error = false;
+  //   if (error) {
+  //     // You can return any result of the action.
+  //     // Here, we return only the error.
+  //     return error;
+  //   }
+
+  //   // handle success
+  // }, 0);
+  return (
+    <SiteContext.Provider value={{ name: "Bob" }}>
+      <Suspense fallback={<>loading</>}>
+        <Podcast />
+      </Suspense>
+      <Footer />
+    </SiteContext.Provider>
   );
 }
 
 export default App;
+
+// const container = document.getElementById("app");
+// if (!container) throw new Error("no container found");
