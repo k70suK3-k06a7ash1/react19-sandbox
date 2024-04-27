a573d4a7b220b7e00cb47d6277baa1baa1315232
2814109 <2814109k@gmail.com>
2024-04-27 13:41:49 +0900

    update react19

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useActionState, useOptimistic } from "types-react";
+import { useState, useActionState, useOptimistic } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
