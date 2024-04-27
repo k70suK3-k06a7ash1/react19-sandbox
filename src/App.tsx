6f17714b5aa0f6773fe31a7cbd6927bc191b9b23
2814109 <2814109k@gmail.com>
2024-04-27 18:41:58 +0900

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,10 @@
 // @ts-ignore
 import { useActionState, useOptimistic, use, cache } from "react";
-import { Effect, Either } from "effect";
-import { Schema } from "@effect/schema";
-import { createContext, Suspense } from "react";
+import { Suspense } from "react";
+import { Podcast } from "./components/Podcast";
+import { Footer } from "./components/Footer";
+import { SiteContext } from "./contexts/SiteContenxt";
 
-const SiteContext = createContext({ name: "" });
-
-const ApiResponse = Schema.Struct({ title: Schema.String });
-
-interface ApiResponse extends Schema.Schema.Type<typeof ApiResponse> {}
-
-const program: Effect.Effect<ApiResponse, Error> = Effect.gen(function* (_) {
-  const response = yield* Effect.tryPromise({
-    try: () => fetch("https://syntax.fm/api/shows/latest"),
-    catch: () => new Error("failed"),
-  });
-  if (!response.ok) {
-    return yield* Effect.fail(new Error());
-  }
-  const json = yield* Effect.tryPromise({
-    try: () => response.json(),
-    catch: () => new Error("error"),
-  });
-  return yield* _(
-    json,
-    Schema.decodeUnknownEither(ApiResponse),
-    Either.mapLeft(() => new Error())
-  );
-});
-
-function Footer() {
-  const { name } = use(SiteContext);
-  return (
-    <footer>
-      <p>©{name}</p>
-    </footer>
-  );
-}
-
-const Podcast = () => {
-  const podcast = use<ApiResponse>(Effect.runPromise(program));
-
-  return (
-    <>
-      <h2>{podcast.title}</h2>
-      <meta property="og:title" content={podcast.title} />
-    </>
-  );
-};
 function App() {
   return (
     // @ts-ignore
